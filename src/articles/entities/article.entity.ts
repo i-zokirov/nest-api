de1203d84aa87b48b02aaeba3d3987af9761f0c6
i-zokirov <izokirov@outlook.com>
2022-10-03 08:55:34 +0200

    Added short readme

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -11,16 +11,12 @@ import {
 export class ArticleEntity {
   @PrimaryGeneratedColumn()
   id: number;
-
   @Column({ default: '' })
   title: string;
-
   @Column({ default: '' })
   body: string;
-
   @CreateDateColumn()
   createdAt: Date;
-
   @ManyToOne(() => UserEntity, (userEntity) => userEntity.articles)
   author: UserEntity;
 }
